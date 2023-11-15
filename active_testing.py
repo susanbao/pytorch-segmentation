b7096aa01bc9899dccdcbb5a80661638703ac078
susanbao <susannju@163.com>
2023-11-15 17:01:29 -0500

    exp

--- active_testing.py
+++ active_testing.py
@@ -84,16 +84,35 @@ def run_one_active_test_risk_estimator(true_losses, expected_losses, seed, sampl
     loss_risk = (sampled_true_losses * risk_estimator_weights).mean()
     return float(loss_risk)
 
+# def active_testing(file_path, true_losses, expected_losses, active_test_type, sample_size_set, display = False):
+#     json_object = {}
+#     for sample_size in sample_size_set:
+#         for seed in random_seed_set:
+#             result = {"active_test_type": active_test_type, "sample_size": sample_size}
+#             loss_risk = run_one_active_test_risk_estimator(true_losses, expected_losses, seed, sample_size)
+#             result["loss"] = loss_risk
+#             json_object[len(json_object)] = result
+#         if display:
+#             print(f"Complete simple size : {sample_size}")
+#     with open(file_path, "w") as outfile:
+#         json.dump(json_object, outfile)
+        
 def active_testing(file_path, true_losses, expected_losses, active_test_type, sample_size_set, display = False):
     json_object = {}
-    for sample_size in sample_size_set:
-        for seed in random_seed_set:
+    for seed in random_seed_set:
+        torch.manual_seed(seed)
+        np.random.seed(seed)
+        random.seed(seed)
+        pick_sample_idxs, weights = acquire(expected_losses, sample_size_set[-1])
+        for sample_size in sample_size_set:
             result = {"active_test_type": active_test_type, "sample_size": sample_size}
-            loss_risk = run_one_active_test_risk_estimator(true_losses, expected_losses, seed, sample_size)
+            risk_estimator_weights = LURE_weights_for_risk_estimator(weights[:sample_size], expected_losses.size)
+            sampled_true_losses = true_losses[pick_sample_idxs[:sample_size]]
+            loss_risk = (sampled_true_losses * risk_estimator_weights).mean()
             result["loss"] = loss_risk
             json_object[len(json_object)] = result
         if display:
-            print(f"Complete simple size : {sample_size}")
+            print(f"Complete seed : {seed}")
     with open(file_path, "w") as outfile:
         json.dump(json_object, outfile)
         
@@ -110,13 +129,31 @@ def main(args):
         model_origin_folder = model_dataset
     base_path = f"./pro_data/{model_origin_folder}/{split}/"
     data_type = args.data_type # image, region_8, region_16
-    store_main_folder = "runs_3"
+    store_main_folder = "runs_3_large_range" # runs_3, runs_3_large_range, runs_10_large_range
     check_folder_exist(f"./results/{store_main_folder}/{model_dataset}")
+    
+    step_list = {5000,10000,15000,20000}
+    # step_list = {25000}
+    # step_list = {35000}
+    # step_list = {25000, 30000, 35000, 40000, 45000, 50000, 55000, 60000}
+    # step_list = {25000,30000, 35000, 40000, 45000,55000, 65000, 70000, 75000, 80000,85000,90000,95000, 100000}
+    # step_list = np.arange(25000, 60001, 5000)
+    # step_list = {65000, 70000, 75000, 80000,85000,90000,95000, 100000}
+    
+    baseline_run = True
+    
     if data_type == "image":
         true_losses = np_read(base_path + "image_true_losses.npy")
         # sample_size_precentage = [0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008, 0.009, 0.01, 0.015, 0.02, 0.025, 0.03, 0.035, 0.04, 0.045,
         #                       0.05, 0.055, 0.06, 0.065, 0.07, 0.075, 0.08]
-        sample_size_precentage = np.linspace(0.01, 0.08, 20)
+        if store_main_folder == "runs_3":
+            sample_size_precentage = np.linspace(0.01, 0.08, 20)
+        else:
+            if model_origin_folder[-4:] == "CITY":
+                sample_size_precentage = np.linspace(0.001, 0.5, 500)
+                sample_size_precentage = sample_size_precentage[2:]
+            else:
+                sample_size_precentage = np.linspace(0.001, 0.5, 500)
         result_json_path = f"./results/{store_main_folder}/{model_dataset}/image_based_active_testing/"
         vit_base_path = "../ViT-pytorch/output/"
     if data_type == "image_2":
@@ -134,7 +171,10 @@ def main(args):
     elif data_type == "region_16":
         true_losses = np_read(base_path + "region_16_16_true_losses.npy")
         # sample_size_precentage = np.linspace(0.0001, 0.001, 20)
-        sample_size_precentage = np.linspace(0.0002, 0.001, 20)
+        if store_main_folder == "runs_3":
+            sample_size_precentage = np.linspace(0.0002, 0.001, 20)
+        else:
+            sample_size_precentage = np.linspace(0.00001, 0.01, 1000)
         result_json_path = f"./results/{store_main_folder}/{model_dataset}/region_16_16_active_testing/"
         # vit_base_path = "../ViT-pytorch/output/region_16_16/"
         vit_base_path = "../ViT-pytorch/output/"
@@ -151,7 +191,10 @@ def main(args):
     elif data_type == "region_32":
         true_losses = np_read(base_path + "region_32_32_true_losses.npy")
         # sample_size_precentage = np.linspace(0.0001, 0.001, 20)
-        sample_size_precentage = np.linspace(0.0002, 0.001, 20)
+        if store_main_folder == "runs_3":
+            sample_size_precentage = np.linspace(0.0002, 0.001, 20)
+        else:
+            sample_size_precentage = np.linspace(0.00001, 0.01, 1000)
         result_json_path = f"./results/{store_main_folder}/{model_dataset}/region_32_32_active_testing/"
         vit_base_path = "../ViT-pytorch/output/"
 
@@ -159,28 +202,27 @@ def main(args):
     box_labels_nums = true_losses.shape[0]
     sample_size_set = (np.array(sample_size_precentage) * box_labels_nums).astype(int).tolist()
     
-    ## random sample
-    file_path = result_json_path + "random_sample_3_runs.json"
-    json_object = {}
-    for sample_size in sample_size_set:
-        for seed in random_seed_set:
-            result = {"active_test_type": "random sample", "sample_size": sample_size}
-            loss_risk = run_one_random_sample_risk_estimator(true_losses, seed, sample_size)
-            result["loss"] = float(loss_risk)
-            json_object[len(json_object)] = result
-    write_one_results(json_object, file_path)
+    if baseline_run:
+        ## random sample
+        file_path = result_json_path + "random_sample_3_runs.json"
+        json_object = {}
+        for sample_size in sample_size_set:
+            for seed in random_seed_set:
+                result = {"active_test_type": "random sample", "sample_size": sample_size}
+                loss_risk = run_one_random_sample_risk_estimator(true_losses, seed, sample_size)
+                result["loss"] = float(loss_risk)
+                json_object[len(json_object)] = result
+        write_one_results(json_object, file_path)
+
+        # whole dataset
+        file_path = result_json_path + "None.json"
+        result = {"active_test_type": "None", "sample_size": true_losses.size}
+        result["loss"] = get_whole_data_set_risk_estimator(true_losses)
+        json_object = {}
+        json_object[0] = result
+        with open(file_path, "w") as outfile:
+            json.dump(json_object, outfile)
     
-    # whole dataset
-    file_path = result_json_path + "None.json"
-    result = {"active_test_type": "None", "sample_size": true_losses.size}
-    result["loss"] = get_whole_data_set_risk_estimator(true_losses)
-    json_object = {}
-    json_object[0] = result
-    with open(file_path, "w") as outfile:
-        json.dump(json_object, outfile)
-    
-    step_list = {5000,10000,15000,20000}
-    # step_list = {5000,15000}
     
     ase_store_path = f"./ase_results/{model_origin_folder}/"
     if data_type == "image":
@@ -188,39 +230,41 @@ def main(args):
             val_estimated_loss = np.array(read_one_results(f"../ViT-pytorch/output/ViT_{model_dataset}_all_losses_{train_step}.json")['losses'])
             file_path = result_json_path + f"ViT_all_runs_{train_step}.json"
             active_testing(file_path, true_losses, val_estimated_loss, "ViT all", sample_size_set)
-        at_losses = np_read(ase_store_path + f"AT_{model_origin_folder}_image_Q.npy")
-        file_path = result_json_path + f"AT_runs.json"
-        active_testing(file_path, true_losses, at_losses, "AT image", sample_size_set)
-        
-        ase_losses = np_read(ase_store_path + f"ASE_{model_origin_folder}_image_Q.npy")
-        file_path = result_json_path + f"ASE_runs.json"
-        active_testing(file_path, true_losses, ase_losses, "ASE image", sample_size_set)
+        if baseline_run:
+            at_losses = np_read(ase_store_path + f"AT_{model_origin_folder}_image_Q.npy")
+            file_path = result_json_path + f"AT_runs.json"
+            active_testing(file_path, true_losses, at_losses, "AT image", sample_size_set)
+
+            ase_losses = np_read(ase_store_path + f"ASE_{model_origin_folder}_image_Q.npy")
+            file_path = result_json_path + f"ASE_runs.json"
+            active_testing(file_path, true_losses, ase_losses, "ASE image", sample_size_set)
     elif data_type == "region_16":
         for train_step in step_list:
             expected_losses = np.array(read_one_results(vit_base_path + f"ViT_{model_dataset}_region_losses_{train_step}.json")['losses']).squeeze()
+            # expected_losses = np.array(read_one_results(vit_base_path + f"ViT_{model_dataset}_all_region_losses_{train_step}.json")['losses']).squeeze()
             file_path = result_json_path + f"ViT_region_runs_{train_step}.json"
             active_testing(file_path, true_losses, expected_losses, "ViT region", sample_size_set, display=False)
-            
-        at_losses = np_read(ase_store_path + f"AT_{model_origin_folder}_region_16_Q.npy")
-        file_path = result_json_path + f"AT_runs.json"
-        active_testing(file_path, true_losses, at_losses, "AT region", sample_size_set)
-        
-        ase_losses = np_read(ase_store_path + f"ASE_{model_origin_folder}_region_16_Q.npy")
-        file_path = result_json_path + f"ASE_runs.json"
-        active_testing(file_path, true_losses, ase_losses, "ASE region", sample_size_set)
+        if baseline_run:    
+            at_losses = np_read(ase_store_path + f"AT_{model_origin_folder}_region_16_Q.npy")
+            file_path = result_json_path + f"AT_runs.json"
+            active_testing(file_path, true_losses, at_losses, "AT region", sample_size_set)
+            ase_losses = np_read(ase_store_path + f"ASE_{model_origin_folder}_region_16_Q.npy")
+            file_path = result_json_path + f"ASE_runs.json"
+            active_testing(file_path, true_losses, ase_losses, "ASE region", sample_size_set)
     elif data_type == "region_32":
         for train_step in step_list:
             expected_losses = np.array(read_one_results(vit_base_path + f"ViT_{model_dataset}_region_32_32_losses_{train_step}.json")['losses']).squeeze()
             file_path = result_json_path + f"ViT_region_runs_{train_step}.json"
             active_testing(file_path, true_losses, expected_losses, "ViT region 32", sample_size_set, display=False)
-            
-        at_losses = np_read(ase_store_path + f"AT_{model_origin_folder}_region_32_Q.npy")
-        file_path = result_json_path + f"AT_runs.json"
-        active_testing(file_path, true_losses, at_losses, "AT region 32", sample_size_set)
         
-        ase_losses = np_read(ase_store_path + f"ASE_{model_origin_folder}_region_32_Q.npy")
-        file_path = result_json_path + f"ASE_runs.json"
-        active_testing(file_path, true_losses, ase_losses, "ASE region 32", sample_size_set)
+        if baseline_run:
+            at_losses = np_read(ase_store_path + f"AT_{model_origin_folder}_region_32_Q.npy")
+            file_path = result_json_path + f"AT_runs.json"
+            active_testing(file_path, true_losses, at_losses, "AT region 32", sample_size_set)
+
+            ase_losses = np_read(ase_store_path + f"ASE_{model_origin_folder}_region_32_Q.npy")
+            file_path = result_json_path + f"ASE_runs.json"
+            active_testing(file_path, true_losses, ase_losses, "ASE region 32", sample_size_set)
             
     
 if __name__ == "__main__":
--- active_testing.sh
+++ active_testing.sh
@@ -1,77 +1,104 @@
 # python active_testing.py --model_data_type UNet_VOC --data_type image
 # python active_testing.py --model_data_type PSPNet_VOC --data_type image
-# python active_testing.py --model_data_type DeepLab_VOC --data_type image
 # python active_testing.py --model_data_type FCN_VOC --data_type image
 # python active_testing.py --model_data_type SEGNet_VOC --data_type image
+# python active_testing.py --model_data_type PSPNet_VOC_40 --data_type image
+# python active_testing.py --model_data_type PSPNet_VOC_90 --data_type image
+# python active_testing.py --model_data_type PSPNet_VOC_60 --data_type image
+# python active_testing.py --model_data_type PSPNet_VOC_70 --data_type image
+# python active_testing.py --model_data_type PSPNet_VOC_30 --data_type image
+# python active_testing.py --model_data_type PSPNet_VOC_80 --data_type image
 
 # python active_testing.py --model_data_type PSPNet_CITY --data_type image
 # python active_testing.py --model_data_type UNet_CITY --data_type image
 # python active_testing.py --model_data_type SEGNet_CITY --data_type image
-# python active_testing.py --model_data_type DeepLab_CITY --data_type image
 # python active_testing.py --model_data_type FCN_CITY --data_type image
+# python active_testing.py --model_data_type PSPNet_CITY_40 --data_type image
+# python active_testing.py --model_data_type PSPNet_CITY_90 --data_type image
 
-# python active_testing.py --model_data_type UNet_VOC --data_type region_32
+# # python active_testing.py --model_data_type UNet_VOC --data_type region_32
 # python active_testing.py --model_data_type PSPNet_VOC --data_type region_32
-# python active_testing.py --model_data_type DeepLab_VOC --data_type region_32
 # python active_testing.py --model_data_type SEGNet_VOC --data_type region_32
 # python active_testing.py --model_data_type FCN_VOC --data_type region_32
+# python active_testing.py --model_data_type UNet_VOC_40 --data_type region_32
+# python active_testing.py --model_data_type UNet_VOC_90 --data_type region_32
+# python active_testing.py --model_data_type FCN_VOC_40 --data_type region_32
+# python active_testing.py --model_data_type FCN_VOC_90 --data_type region_32
 
 # python active_testing.py --model_data_type PSPNet_CITY --data_type region_32
 # python active_testing.py --model_data_type UNet_CITY --data_type region_32
-# python active_testing.py --model_data_type DeepLab_CITY --data_type region_32
 # python active_testing.py --model_data_type SEGNet_CITY --data_type region_32
 # python active_testing.py --model_data_type FCN_CITY --data_type region_32
 
-# python active_testing.py --model_data_type UNet_VOC --data_type region_16
+# # python active_testing.py --model_data_type UNet_VOC --data_type region_16
 # python active_testing.py --model_data_type PSPNet_VOC --data_type region_16
-# python active_testing.py --model_data_type DeepLab_VOC --data_type region_16
 # python active_testing.py --model_data_type FCN_VOC --data_type region_16
 # python active_testing.py --model_data_type SEGNet_VOC --data_type region_16
 
 # python active_testing.py --model_data_type PSPNet_CITY --data_type region_16
 # python active_testing.py --model_data_type UNet_CITY --data_type region_16
 # python active_testing.py --model_data_type SEGNet_CITY --data_type region_16
-# python active_testing.py --model_data_type DeepLab_CITY --data_type region_16
 # python active_testing.py --model_data_type FCN_CITY --data_type region_16
-
-# python active_testing.py --model_data_type FCN_COCO --data_type image
-# python active_testing.py --model_data_type FCN_COCO --data_type region_32
-# python active_testing.py --model_data_type FCN_COCO --data_type region_16
+# python active_testing.py --model_data_type UNet_CITY_30 --data_type region_16
+# python active_testing.py --model_data_type UNet_CITY_40 --data_type region_16
+python active_testing.py --model_data_type UNet_CITY_60 --data_type region_16
+python active_testing.py --model_data_type UNet_CITY_70 --data_type region_16
+python active_testing.py --model_data_type UNet_CITY_10 --data_type region_16
+python active_testing.py --model_data_type UNet_CITY_20 --data_type region_16
 
 # python active_testing.py --model_data_type PSPNet_COCO --data_type image
+# python active_testing.py --model_data_type UNet_COCO --data_type image
+# python active_testing.py --model_data_type SEGNet_COCO --data_type image
+# python active_testing.py --model_data_type FCN_COCO --data_type image
 
-# python active_testing.py --model_data_type SEGNet_ADE20K --data_type image
+# python active_testing.py --model_data_type PSPNet_COCO_40 --data_type image
+# python active_testing.py --model_data_type PSPNet_COCO_90 --data_type image
+# python active_testing.py --model_data_type PSPNet_COCO_30 --data_type image
+# python active_testing.py --model_data_type SEGNet_COCO_90 --data_type image
+python active_testing.py --model_data_type PSPNet_COCO_60 --data_type image
+python active_testing.py --model_data_type PSPNet_COCO_80 --data_type image
+
 # python active_testing.py --model_data_type PSPNet_ADE20K --data_type image
 # python active_testing.py --model_data_type UNet_ADE20K --data_type image
+# python active_testing.py --model_data_type SEGNet_ADE20K --data_type image
 # python active_testing.py --model_data_type FCN_ADE20K --data_type image
 
+# python active_testing.py --model_data_type FCN_ADE20K_90 --data_type image
+# python active_testing.py --model_data_type PSPNet_ADE20K_40 --data_type image
+# python active_testing.py --model_data_type PSPNet_ADE20K_70 --data_type image
 
 # python active_testing.py --model_data_type PSPNet_COCO --data_type region_32
+# python active_testing.py --model_data_type SEGNet_COCO --data_type region_32
+# python active_testing.py --model_data_type UNet_COCO --data_type region_32
+# python active_testing.py --model_data_type FCN_COCO --data_type region_32
 
-# python active_testing.py --model_data_type SEGNet_ADE20K --data_type region_32
+
 # python active_testing.py --model_data_type PSPNet_ADE20K --data_type region_32
 # python active_testing.py --model_data_type UNet_ADE20K --data_type region_32
+# python active_testing.py --model_data_type SEGNet_ADE20K --data_type region_32
+# python active_testing.py --model_data_type FCN_ADE20K_90 --data_type region_32
 
 
 # python active_testing.py --model_data_type PSPNet_COCO --data_type region_16
-
-# python active_testing.py --model_data_type SEGNet_ADE20K --data_type region_16
-# python active_testing.py --model_data_type PSPNet_ADE20K --data_type region_16
-# python active_testing.py --model_data_type UNet_ADE20K --data_type region_16
-
-
-# python active_testing.py --model_data_type SEGNet_COCO --data_type image
-# python active_testing.py --model_data_type SEGNet_COCO --data_type region_32
 # python active_testing.py --model_data_type SEGNet_COCO --data_type region_16
+# python active_testing.py --model_data_type UNet_COCO --data_type region_16
+# python active_testing.py --model_data_type FCN_COCO --data_type region_16
 
-# python active_testing.py --model_data_type FCN_ADE20K_90 --data_type image
-
-# python active_testing.py --model_data_type SEGNet_COCO_90 --data_type image
-
-
-# python active_testing.py --model_data_type FCN_ADE20K_90 --data_type region_16
 # python active_testing.py --model_data_type SEGNet_COCO_90 --data_type region_16
 
-# python active_testing.py --model_data_type FCN_ADE20K_90 --data_type region_32
+# python active_testing.py --model_data_type PSPNet_ADE20K --data_type region_16
+# python active_testing.py --model_data_type UNet_ADE20K --data_type region_16
+# python active_testing.py --model_data_type SEGNet_ADE20K --data_type region_16
+# python active_testing.py --model_data_type FCN_ADE20K_90 --data_type region_16
+# python active_testing.py --model_data_type FCN_ADE20K --data_type region_16
 
-python active_testing.py --model_data_type PSPNet_COCO_40 --data_type image
+# python active_testing.py --model_data_type SEGNet_VOC_40 --data_type region_16
+# python active_testing.py --model_data_type SEGNet_VOC_90 --data_type region_16
+# python active_testing.py --model_data_type SEGNet_VOC_30 --data_type region_16
+# python active_testing.py --model_data_type SEGNet_VOC_80 --data_type region_16
+
+# python active_testing.py --model_data_type FCN_CITY_40 --data_type region_16
+
+
+
+
